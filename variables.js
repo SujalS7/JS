a097c6dcfefb2a4dbd71feb8c0a3c68e9b415a8d
SujalS7 <sujalshinde1264@gmail.com>
2025-07-08 15:04:14 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,7 @@
+const accountID = 12345
+let accountEmail = "SS@gmail.com"
+var accountName = "ss" //rarely use var cuz of block and function scope issues
+                       // Use let for variables that might change, const for constants
+let accountCity
+
+console.log([accountID, accountEmail, accountName, accountCity]);
