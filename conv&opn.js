6cbadd713aac6bbc2b6666eda3a0ee19b1e6f2d8
SujalS7 <sujalshinde1264@gmail.com>
2025-07-09 13:53:22 +0530

    conversions

---
+++ conv&opn.js
@@ -0,0 +1,16 @@
+ let someNum = 5;
+ let numtostring = String(someNum);
+ console.log(numtostring); 
+ console.log(typeof numtostring)
+// Output: "5"
+// Output: "string"
+// This converts a number to a string in JavaScript.
+
+let IsHuman = true;
+let booleanToString = Number(IsHuman);
+console.log(booleanToString);   
+console.log(typeof booleanToString)
+// Output: 1
+// Output: "number"
+// This converts a boolean to a number in JavaScript.
+ 
