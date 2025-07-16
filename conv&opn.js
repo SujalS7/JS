b25f1be91b4551e39310a888d2975fcbf6de6d01
SujalS7 <sujalshinde1264@gmail.com>
2025-07-16 14:53:24 +0530

    New changes

--- conv&opn.js
+++ conv&opn.js
@@ -1,16 +1,17 @@
- let someNum = 5;
- let numtostring = String(someNum);
- console.log(numtostring); 
- console.log(typeof numtostring)
+let someNum = 5;
+let numtostring = String(someNum);
+console.log(numtostring); 
+console.log(typeof numtostring)
 // Output: "5"
 // Output: "string"
 // This converts a number to a string in JavaScript.
 
 let IsHuman = true;
 let booleanToString = Number(IsHuman);
-console.log(booleanToString);   
-console.log(typeof booleanToString)
+console.log(booleanToString);
+console.log(typeof booleanToString);
 // Output: 1
 // Output: "number"
 // This converts a boolean to a number in JavaScript.
- 
+
+/* operations */
